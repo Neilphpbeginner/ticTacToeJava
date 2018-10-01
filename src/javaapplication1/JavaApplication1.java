64c97762e90e12fd9dcf8065ba36b05e293a688a
Neil <lemmer.neil@gmail.com>
2018-10-01 06:40:32 +0200

    Second Commit

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -5,12 +5,13 @@ import java.util.Scanner;
 public class JavaApplication1 {
 
     static String[][] ticTacToe = new String[3][3];
-    Scanner playerX = new Scanner(System.in);
-        static int playerXRow;
-        static int playerXColomn;
-        static Scanner playerO = new Scanner(System.in);
-        static int playerORow;
-        static int playerOColomn;
+    static Scanner playerX = new Scanner(System.in);
+    static boolean winner = false;
+    static int playerXRow;
+    static int playerXColomn;
+    static Scanner playerO = new Scanner(System.in);
+    static int playerORow;
+    static int playerOColomn;
 
     public static void initBoard() {
         for (int i = 0; i < ticTacToe.length; i++) {
@@ -18,14 +19,19 @@ public class JavaApplication1 {
                 ticTacToe[i][j] = "  ";
             }
         }
+    }
 
+    public static boolean checkWinner(String[][] winner) {
+        if(winner[0][0] == "x" && winner[0][1] == "x" && winner[0][2] == "x"){
+            return true;
+        }
+        return false;
     }
 
     public static void main(String[] args) {
 
-        
         initBoard();
-        boolean winner = false;
+        
 
         while (winner != true) {
             for (int i = 0; i < ticTacToe.length; i++) {
@@ -34,15 +40,25 @@ public class JavaApplication1 {
                         System.out.println("--------------");
                         System.out.println("| " + ticTacToe[i][0] + " | " + ticTacToe[i][1] + " | " + ticTacToe[i][2] + " |");
                     }
-
                 }
             }
+            
             System.out.println("--------------");
-            System.out.print("Player X, please enter a row (0, 1 or 2): ");
-            playerXRow = playerX.nextInt();
-            System.out.println("Player X, please enter a column (0, 1 or 2): ");
-            playerXColomn = playerX.nextInt();
-            ticTacToe[playerXRow][playerXColomn] = "X";
+            if (checkWinner(ticTacToe)) {
+                System.out.print("Player X, please enter a row (0, 1 or 2): ");
+                playerXRow = playerX.nextInt();
+                System.out.print("Player X, please enter a column (0, 1 or 2):");
+                playerXColomn = playerX.nextInt();
+                ticTacToe[playerXRow][playerXColomn] = "X";
+            } else {
+                System.out.print("Player O, please enter a row (0, 1 or 2): ");
+                playerORow = playerO.nextInt();
+                System.out.print("Player O, please enter a column (0, 1 or 2):");
+                playerOColomn = playerO.nextInt();
+                ticTacToe[playerORow][playerOColomn] = "O";
+
+            }
+//          
         }
     }
 
